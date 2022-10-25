c909e2a7e0b38c560888ffdc8cff0468080035f5
Diana Murtazina <murka8831@yandex.ru>
2022-10-25 21:42:54 +0400

    transmission of props

--- pizza/src/App.js
+++ pizza/src/App.js
@@ -16,7 +16,7 @@ function App() {
                     </div>
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
-                        <PizzaBlock />
+                        <PizzaBlock title="Мексиканская" price={500} />
                         <PizzaBlock />
                         <PizzaBlock />
                         <PizzaBlock />
