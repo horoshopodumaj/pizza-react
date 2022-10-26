5b53ae9aa39d7fa4fc83b6750f44e624d1a0b71d
Diana Murtazina <murka8831@yandex.ru>
2022-10-26 23:54:15 +0400

    working with a list of categories

--- pizza/src/components/Categories.js
+++ pizza/src/components/Categories.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
+const categories = [
+    { id: 1, title: "Все" },
+    { id: 2, title: "Мясные" },
+    { id: 3, title: "Вегетарианская" },
+    { id: 4, title: "Гриль" },
+    { id: 5, title: "Острые" },
+    { id: 6, title: "Закрытые" },
+];
 function Categories() {
+    const [activeCategory, setActiveCategory] = useState(0);
+
+    const onClickCategory = (id) => {
+        setActiveCategory(id);
+    };
     return (
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                {categories.map((category) => (
+                    <li
+                        onClick={() => onClickCategory(category.id)}
+                        className={activeCategory === category.id ? "active" : ""}
+                        key={category.id}>
+                        {category.title}
+                    </li>
+                ))}
             </ul>
         </div>
     );
