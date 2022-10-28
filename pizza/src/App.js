531c0cddd75963b1316fe724d05ff2924158e409
Diana Murtazina <murka8831@yandex.ru>
2022-10-28 22:35:05 +0400

    go to the shopping cart page by clicking on the icon

--- pizza/src/App.js
+++ pizza/src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
@@ -14,6 +15,7 @@ function App() {
                     <Routes>
                         <Route path="" element={<Home />} />
                         <Route path="*" element={<NotFound />} />
+                        <Route path="cart" element={<Cart />} />
                     </Routes>
                 </div>
             </div>
