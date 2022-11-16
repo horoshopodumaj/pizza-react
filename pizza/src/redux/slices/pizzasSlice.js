f6209aa14f332327e59484cb4993fece0162c0ee
Diana Murtazina <murka8831@yandex.ru>
2022-11-16 23:41:19 +0400

    get pizzas

--- pizza/src/redux/slices/pizzasSlice.js
+++ pizza/src/redux/slices/pizzasSlice.js
@@ -11,6 +11,7 @@ export const fetchPizzas = createAsyncThunk("pizza/fetchPizzasStatus", async (pa
 
 const initialState = {
     items: [],
+    status: "loading", // loading, success, error
 };
 
 const pizzasSlice = createSlice({
@@ -19,6 +20,20 @@ const pizzasSlice = createSlice({
     reducers: {
         setItems(state, action) {
             state.items = action.payload;
+        },
+    },
+    extraReducers: {
+        [fetchPizzas.pending]: (state) => {
+            state.status = "loading";
+            state.items = [];
+        },
+        [fetchPizzas.fulfilled]: (state, action) => {
+            state.items = action.payload;
+            state.status = "succes";
+        },
+        [fetchPizzas.rejected]: (state) => {
+            state.status = "error";
+            state.items = [];
         },
     },
 });
