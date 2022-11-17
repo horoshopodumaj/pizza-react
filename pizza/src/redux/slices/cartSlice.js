1d8f4bbf47428a3b2586e97e4da62ef73737acb2
Diana Murtazina <murka8831@yandex.ru>
2022-11-17 14:39:04 +0400

    creating and passing a selector to the pizzablock component (for training)

--- pizza/src/redux/slices/cartSlice.js
+++ pizza/src/redux/slices/cartSlice.js
@@ -43,6 +43,7 @@ export const cartSlice = createSlice({
 });
 
 export const selectCart = (state) => state.cart;
+export const selectCartItemById = (id) => (state) => state.cart.items.find((obj) => obj.id === id);
 
 export const { addItem, removeItem, clearCart, minusItem } = cartSlice.actions;
 
