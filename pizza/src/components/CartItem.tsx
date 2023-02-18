e72484a0c132ac5f6d4a56dce025553aa55a1a23
Diana Murtazina <murka8831@yandex.ru>
2023-02-18 16:52:45 +0400

    component typing

--- pizza/src/components/CartItem.tsx
+++ pizza/src/components/CartItem.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { addItem, minusItem, removeItem } from "../redux/slices/cartSlice";
 
-const CartItem = ({ id, title, type, price, size, count, imageUrl }) => {
+type CartItemProps = {
+    id: string;
+    title: string;
+    type: string;
+    price: number;
+    size: number;
+    count: number;
+    imageUrl: string;
+};
+const CartItem: React.FC<CartItemProps> = ({ id, title, type, price, size, count, imageUrl }) => {
     const dispatch = useDispatch();
 
     const onClickPlus = () => {
