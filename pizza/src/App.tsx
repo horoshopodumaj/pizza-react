8a364be1c45cede8447b89cec9c0f6063b438294
Diana Murtazina <murka8831@yandex.ru>
2023-02-19 19:44:51 +0400

    used react-loadable library

--- pizza/src/App.tsx
+++ pizza/src/App.tsx
@@ -2,9 +2,15 @@ import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
+import Loadable from "react-loadable";
 import "./scss/app.scss";
 
-const Cart = React.lazy(() => import(/* webpackChunkName: "Cart" */ "./pages/Cart"));
+//const Cart = React.lazy(() => import(/* webpackChunkName: "Cart" */ "./pages/Cart"));
+
+const Cart = Loadable({
+    loader: () => import(/* webpackChunkName: "Cart" */ "./pages/Cart"),
+    loading: () => <div>Loading...</div>,
+});
 const FullPizza = React.lazy(() => import(/* webpackChunkName: "FullPizza" */ "./pages/FullPizza"));
 const NotFound = React.lazy(() => import(/* webpackChunkName: "NotFound" */ "./pages/NotFound"));
 
@@ -21,14 +27,7 @@ function App() {
                         </Suspense>
                     }
                 />
-                <Route
-                    path="cart"
-                    element={
-                        <Suspense fallback={<div>Loading...</div>}>
-                            <Cart />
-                        </Suspense>
-                    }
-                />
+                <Route path="cart" element={<Cart />} />
                 <Route
                     path="*"
                     element={
